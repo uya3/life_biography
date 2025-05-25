45e0c3c6f99c357aaa989e0b54199ad9239ec29f
David Wang <sit765@gmail.com>
2025-05-25 16:44:01 -0400

    updated timeline

--- summarize.py
+++ summarize.py
@@ -1,12 +1,24 @@
 from google import genai
+from google.genai import types
 from prompts import prompts_db
 
 client = genai.Client(api_key="GEMINI_API_KEY")
 
-def generate_summary(uuid, text):
-    
+
+def generate_summaries(uuid, bio):
+    for prompt in prompts_db:
+        if prompt['name'] == uuid:
+            text = prompt['text']
+            break
+
+def generate_summary(uuid, bio, prompt):
+    config=types.GenerateContentConfig(
+        max_output_tokens=5000,
+        temperature=0.1
+    )
     response = client.models.generate_content(
         model="gemini-2.5-flash",
+        config=config,
         contents=text,
     )
     return response.text
