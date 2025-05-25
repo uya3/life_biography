e6de722e74970e81a72e9f157907119c8c2b8ed2
David Wang <sit765@gmail.com>
2025-05-25 17:32:20 -0400

    active prompts

---
+++ example_summarize.py
@@ -0,0 +1,17 @@
+import summarize
+
+test_bio = """* B. Childhood Memories and Early Interests
+* 1. Wartime Sydney (Cremorne, Manly Beach, rock pools, ferries, oil slicks)
+* 2. Tasmanian Interludes (10 Carnarvon Street, making mud, dog Sidi)
+* 3. Early Scientific Curiosity (Steam engines, father's light box, fascination with how things work)
+* 4. Development of Myopia
+* 5. Early Encounters with Music (Beethoven symphonies on 78rpm, father's oscilloscope, Miss Budden's school piano)
+* 6. Learning to Read (Angus and the Cat, Pretzel, "eureka moment")
+* 7. Fascination with Language ("Between the bars" incident)
+* 8. Father's Influence (Model aeroplanes, explaining the stars)
+* C. Early Schooling
+* 1. Kindergarten in Sydney (Miss Budden's)
+"""
+
+resp = summarize.generate_summaries(1,test_bio)
+print (resp)
--- summarize.py
+++ summarize.py
@@ -1,26 +1,32 @@
+import os
 from google import genai
 from google.genai import types
-from prompts import prompts_db
+import prompts
 
-client = genai.Client(api_key="GEMINI_API_KEY")
+# get GEMINI_API_KEY from environment variable
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=GEMINI_API_KEY)
 
+ACTIVE_PROMPTS = ['activity', 'short']
 
 def generate_summaries(uuid, bio):
-    for prompt in prompts_db:
-        if prompt['name'] == uuid:
-            text = prompt['text']
-            break
+    prompts.load_prompts()
+    responses = {}
+    for prompt in prompts.prompts_db:
+        if prompt['name'] not in ACTIVE_PROMPTS:
+            continue
+        responses[prompt['name']] = generate_summary(uuid, bio, prompt['text'])
+    return responses
 
 def generate_summary(uuid, bio, prompt):
     config=types.GenerateContentConfig(
         max_output_tokens=5000,
         temperature=0.1
     )
+    contents = f'{prompt}: \n {bio}'
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-2.0-flash",
         config=config,
-        contents=f'{prompt}: \n {bio}',
+        contents=contents,
     )
     return response.text
-
-
